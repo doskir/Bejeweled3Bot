e866bc418bd4139bc3e69fd2d0776d579db7179f
Patrick Speiser <doskir@gmail.com>
2012-02-04 18:11:17 +0100

    Added mouse automation, some improvements to the win32 api stuff.

--- Bejeweled3Bot/GameWindow.cs
+++ Bejeweled3Bot/GameWindow.cs
@@ -10,25 +10,46 @@ using Emgu.CV.Structure;
 
 namespace Bejeweled3Bot
 {
-    class GameWindow
+    internal class GameWindow
     {
-        const string WINDOWNAME = "Bejeweled 3";
+        private const string WINDOWNAME = "Bejeweled 3";
+        private const string WINDOWCLASS = "MainWindow";
+
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
         [DllImport("User32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool PrintWindow(IntPtr hwnd, IntPtr hDC, uint nFlags);
+        private static extern bool PrintWindow(IntPtr hwnd, IntPtr hDC, uint nFlags);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
 
-        public Image<Bgr,byte> TakeScreenshot()
+        public enum PostMessageFlags
         {
-            IntPtr windowHandle = FindWindow(null, WINDOWNAME);
+            MK_LBUTTON = 0x0001,
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_ACTIVATEAPP = 0x001C
+        }
+
+        private IntPtr GetGameWindowHandle()
+        {
+            return FindWindow(WINDOWCLASS, WINDOWNAME);
+        }
+
+        public Image<Bgr, byte> TakeScreenshot()
+        {
             Bitmap frame = new Bitmap(1024, 768);
-            Graphics g = Graphics.FromImage(frame);
-            IntPtr dc = g.GetHdc();
-            PrintWindow(windowHandle, dc,1);
-            g.ReleaseHdc();
+            using (Graphics g = Graphics.FromImage(frame))
+            {
+                IntPtr deviceContextHandle = g.GetHdc();
+                PrintWindow(GetGameWindowHandle(), deviceContextHandle, 1);
+                g.ReleaseHdc();
+            }
             //we have the bitmap now
             //turn it into an Image for emgu
             BitmapData bmpData = frame.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), ImageLockMode.ReadWrite,
@@ -41,8 +62,25 @@ namespace Bejeweled3Bot
             //dispose all unused image data to prevent memory leaks
             frame.Dispose();
             tempImage.Dispose();
+            image.Save("screenshot.png");
 
             return image;
+        }
+
+        public void Click(int x, int y)
+        {
+            IntPtr windowHandle = GetGameWindowHandle();
+            var pos = new IntPtr(y*0x10000 + x);
+            SendMessage(windowHandle, (uint) PostMessageFlags.WM_LBUTTONDOWN,
+                        new IntPtr((int) PostMessageFlags.MK_LBUTTON), pos);
+            System.Threading.Thread.Sleep(10);
+            SendMessage(windowHandle, (int) PostMessageFlags.WM_LBUTTONUP, new IntPtr((int) PostMessageFlags.MK_LBUTTON),
+                        pos);
+            System.Threading.Thread.Sleep(10);
+        }
+        public void UnpauseWindow()
+        {
+            SendMessage(GetGameWindowHandle(), 0x001c, (IntPtr) 1, (IntPtr) 0);
         }
     }
 }
